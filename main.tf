90d92b23e277c011e2edc1b59b08efac9a4ac34a
prabaadorsys <51906523+prabaadorsys@users.noreply.github.com>
2025-05-20 17:03:37 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+rovider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "testEC2Instance-wazuh"
+  }
+}
